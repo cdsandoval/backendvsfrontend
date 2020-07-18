4e6cbc6c1d889053a55f5b5f6b97df63a27b77e3
iznomag <iznomag@gmail.com>
2020-07-18 22:15:21 +0200

    Add technologies view

--- frontend/src/app/pages/tecnhologies/tecnhologies.component.ts
+++ frontend/src/app/pages/tecnhologies/tecnhologies.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from 'src/app/services/http.service';
+import { Technology } from 'src/app/models/technology.models';
 
 @Component({
   selector: 'app-tecnhologies',
   templateUrl: './tecnhologies.component.html',
-  styleUrls: ['./tecnhologies.component.css']
+  styleUrls: ['./tecnhologies.component.css'],
 })
 export class TecnhologiesComponent implements OnInit {
-
-  constructor() { }
+  public technologies: Technology[];
+  constructor(public _httpService: HttpService) {}
 
   ngOnInit(): void {
+    this._httpService
+      .getTechnologies()
+      .subscribe((technologies: Technology[]) => {
+        this.technologies = technologies['data'];
+        debugger;
+      });
   }
-
 }
